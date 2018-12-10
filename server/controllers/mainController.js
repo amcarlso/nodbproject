e9bebf39eeebe21338b5c7b47646e7902681fbc1
Allen Carlson <allen.carlson123@gmail.com>
2018-12-10 09:20:36 -0700

    Delete button deletes correct index. Index corrected with edit

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -18,7 +18,7 @@ module.exports = {
   editCountry:(req, res) => {
     console.log(req.body, req.params.id)
     const {country} = req.body
-    const updateId = req.params.id;
+    const updateId = Number(req.params.id);
     const countryIndex = countryList.findIndex(country => {
       if( country.id == updateId) {
         return true
@@ -35,8 +35,10 @@ module.exports = {
   },
   deleteCountry:(req, res) => {
     const deleteID = req.params.id;
-    let countryIndex = countryList.findIndex(country => countryList.id  == deleteID);
-    countryList.splice(countryList[countryIndex], 1);
+    let countryIndex = countryList.findIndex(country => country.id  == deleteID);
+    console.log(countryIndex)
+    console.log(countryList)
+    countryList.splice(countryIndex, 1);
     res.status(200).send(countryList); 
   }
 }
